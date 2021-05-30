75a1c88b1973a4dbe76eab47d139c564391ed483
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-05-30 13:36:23 -0500

    Updated demo files with ability to reset and added more comments

--- Demo/LuanoidServer/init.server.lua
+++ Demo/LuanoidServer/init.server.lua
@@ -1,4 +1,5 @@
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local makeDogu15Rig = require(script:WaitForChild("Dogu15"))
 local applyHumanoidDescription = require(script:WaitForChild("R15HumanoidDescriptionApplier"))
@@ -16,8 +17,28 @@ local function makePlayerCharacter(player)
     character.Parent = workspace
     player.Character = character
 
+    --[[
+        Luanoids use a custom SetNetworkOwner() method due to Roblox's tendancy
+        to reset the NetworkOwner to automatic network ownership which is
+        not favorable for characters.
+
+        Another reason is the NetworkOwner is set
+        as an attribute for clients to know when they are the NetworkOwner due
+        to clients not able to use GetNetworkOwner() which is necessary to
+        determine if they are the currently expected machine to simulate the
+        Luanoid.
+    ]]
     luanoid:SetNetworkOwner(player)
 end
+
+local resetEvent = Instance.new("RemoteEvent")
+resetEvent.Name = "ResetEvent"
+resetEvent.Parent = ReplicatedStorage
+
+resetEvent.OnServerEvent:Connect(function(player)
+    player.Character:Destroy()
+    makePlayerCharacter(player)
+end)
 
 for _,player in pairs(Players:GetPlayers()) do
     makePlayerCharacter(player)
--- Demo/PlayerScriptsLoader.client.lua
+++ Demo/PlayerScriptsLoader.client.lua
@@ -1,7 +1,12 @@
 local Player = game:GetService("Players").LocalPlayer
-local Luanoid = require(game:GetService("ReplicatedStorage"):WaitForChild("Luanoid"))
+local RunService = game:GetService("RunService")
+local StarterGui = game:GetService("StarterGui")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Luanoid = require(ReplicatedStorage:WaitForChild("Luanoid"))
 local PlayerModule = require(script.Parent:WaitForChild("PlayerModule"))
 local Camera = workspace.CurrentCamera
+local ResetEvent = ReplicatedStorage:WaitForChild("ResetEvent")
 
 local Character
 local CurrentLuanoid
@@ -41,13 +46,15 @@ for _,animationData in pairs(ANIMATIONS) do
 end
 
 Player.CharacterAdded:Connect(function(character)
+    local luanoid = Luanoid(nil, character)
+
 	Character = character
 	Camera.CameraSubject = character.PrimaryPart
 	Camera.CameraType = Enum.CameraType.Custom
-	CurrentLuanoid = Luanoid(nil, character)
+	CurrentLuanoid = luanoid
 
 	for animationName, animationData in pairs(ANIMATIONS) do
-        CurrentLuanoid:LoadAnimation(
+        luanoid:LoadAnimation(
             animationData.Animation,
             animationName,
             {
@@ -55,9 +62,41 @@ Player.CharacterAdded:Connect(function(character)
             }
         )
     end
+
+    --[[
+        Luanoid CharacterState is not replicated between the Server and
+        Client, as a result CharacterState related events must be detected
+        on the machine simulating the Luanoid.
+
+        Events such as HealthChanged, Died, and Jumping can be detected through
+        the Luanoid's character attributes.
+    ]]
+    luanoid.StateChanged:Connect(function(newState, oldState)
+        print("CharacterState changed from", oldState.Name, "to", newState.Name)
+    end)
+    luanoid.HealthChanged:Connect(function(health)
+        print("Health is now", health)
+    end)
+    luanoid.Died:Connect(function(isDead)
+        print("Dead is", isDead)
+
+        if isDead then
+            ResetEvent:FireServer()
+        end
+    end)
+    luanoid.FreeFalling:Connect(function(isFreeFalling)
+        print("Freefalling is", isFreeFalling)
+    end)
+    luanoid.Jumping:Connect(function(isJumping)
+        print("Jumping is", isJumping)
+    end)
 end)
 
-game:GetService("RunService").Heartbeat:Connect(function()
+--[[
+    This simply taps into the existing Roblox character scripts and sets the
+    Luanoid's MoveDirection based on the camera every frame.
+]]
+RunService.Heartbeat:Connect(function()
 	if CurrentLuanoid and Character and Character.Parent then
 		local activeController = PlayerModule.controls.activeController
 
@@ -65,10 +104,26 @@ game:GetService("RunService").Heartbeat:Connect(function()
 			local moveVector = activeController.moveVector
 
 			if activeController.jumpRequested then
-				CurrentLuanoid:Jump()
+				CurrentLuanoid.Jump = true
 			end
 
 			CurrentLuanoid.MoveDirection = Camera.CFrame:VectorToWorldSpace(moveVector)
 		end
 	end
+end)
+
+--[[
+    Replaces the default reset logic with one that is compatible for Luanoids.
+]]
+local resetBindable = Instance.new("BindableEvent")
+
+repeat
+    local success = pcall(function()
+        StarterGui:SetCore("ResetButtonCallback", resetBindable)
+    end)
+    RunService.Heartbeat:Wait()
+until success
+
+resetBindable.Event:Connect(function()
+    ResetEvent:FireServer()
 end)
