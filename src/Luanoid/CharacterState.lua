514b0e13b5ee9cf9a82e0ea13d463a39972c7528
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-06-03 23:17:57 -0500

    Updated Enum library

--- src/Luanoid/CharacterState.lua
+++ src/Luanoid/CharacterState.lua
@@ -1,11 +1,11 @@
 return require(script.Parent.Enum).new(
     "CharacterState",
     {
-        Physics = 0,
-        Idling = 1,
-        Walking = 2,
-        Jumping = 3,
-        Falling = 4,
-        Dead = 5,
+        "Physics",
+        "Idling",
+        "Walking",
+        "Jumping",
+        "Falling",
+        "Dead",
     }
 )
