8a6885c818fe5efd1f4f5e61630a49757fe6b57b
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-08-25 23:33:45 -0500

    Optimized more boolean conversions

--- src/Luanoid/init.lua
+++ src/Luanoid/init.lua
@@ -112,7 +112,7 @@ local Luanoid = Class() do
                     HipHeight = luanoidParams.HipHeight or 2,
                     MaxSlopeAngle = luanoidParams.MaxSlopeAngle or 89,
 
-                    AutoRotate = luanoidParams.AutoRotate == nil and true or luanoidParams.AutoRotate,
+                    AutoRotate = luanoidParams.AutoRotate == true,
                     Jump = false,
                 }
             )
@@ -350,7 +350,7 @@ local Luanoid = Class() do
         options = options or {}
         options.Timeout = options.Timeout or 8
         options.DeadzoneRadius = options.DeadzoneRadius or 6
-        options.ResetOnFinish = options.ResetOnFinish == nil and true or options.ResetOnFinish
+        options.ResetOnFinish = options.ResetOnFinish == true
 
         self._moveToTarget = target
         self._moveToTickStart = tick()
